d20a4cbc9a4b920877b6424b2d9ee2658cd2d2b1
santiago villalobos <55002307+SanjeVicor@users.noreply.github.com>
2019-11-08 14:09:24 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 #include <stdlib.h>
@@ -8,15 +7,23 @@
 #include <ctype.h>
 
 int gcd( int $a, int $b )
-
 {
 
     int $result;
 
-    __asm__ __volatile__( "movl %1, %%eax; movl %2, %%ebx;CONTD: cmpl $0, %%ebx;je DONE;xorl %%edx, %%edx; idivl %%ebx;  movl %%ebx, %%eax; movl %%edx, %%ebx; jmp CONTD;  DONE:	movl %%eax, %0;" : "=g" ( $result ) : "g" ( $a ), "g" ( $b )    );
-
+    __asm__ __volatile__(
+        "movl %1, %%eax;"
+        "movl %2, %%ebx;"
+        "CONTD: cmpl $0, %%ebx;"
+        "je DONE;"
+        "xorl %%edx, %%edx;"
+        "idivl %%ebx;"
+        "movl %%ebx, %%eax;"
+        "movl %%edx, %%ebx;"
+        "jmp CONTD;"
+        "DONE: movl %%eax, %0;" : "=g" ( $result ) : "g" ( $a ), "g" ( $b )
+    );
     return $result ;
-
 }
 
 int suma( int $a, int $b )
@@ -59,19 +66,69 @@ void sumatoria(int $count){
     printf("  =  %d  ", $aux);
 }
 
+int resta( int $a, int $b )
+{
+    int $result;
+
+    __asm__ __volatile__( "movl %1, %%eax;movl %2, %%ebx;subl %%ebx, %%eax;movl %%eax, %0;" : "=g" ( $result ) : "g" ( $a ), "g" ( $b )  );
+
+    return $result ;
+
+}
+
+int multiplicacion( int $a, int $b )
+{
+    int $result;
+
+    __asm__ __volatile__( "movl %1, %%eax;movl %2, %%ebx;imul %%ebx;movl %%eax, %0;" : "=g" ( $result ) : "g" ( $a ), "g" ( $b )  );
+
+    return $result ;
+}
+
+int exponencial( int $a, int $b )
+{
+    int $result;
+
+    if($b == 0)
+    {
+        $result = 1;
+    }else if($b == 1)
+    {
+        $result = $a;
+    }else{
+        $b--;
+        __asm__ __volatile__(
+                             " movl %2 , %%ecx ;"
+                              "movl %1, %%eax;"
+                              "movl %1, %%ebx;"
+                              "POW: "
+                              "imul %%ebx;"
+                              "movl %%eax, %0;"
+                              "loop POW;"
+                               : "=g" ( $result ) : "g" ( $a ), "g" ( $b )  );
+    }
+
+    return $result ;
+}
+
 int main()
 {
 
 
 
-    printf("resultado = %d \n\r", gcd(4,2));
+    printf("Maximo Comun Divisor    - resultado = %d \n\r", gcd(8,4));
 
-    printf("resultado = %d \n\r", suma(1000,100));
+    printf("Suma                    - resultado = %d \n\r", suma(1000,-100));
 
-    printf("resultado = %d \n\r", division(10000,100));
+    printf("Resta                   - resultado = %d \n\r", resta(1000,-100));
 
-    printf("Sumatoria de cuadrados = ");sumatoria(20);
+    printf("Multiplicacion          - resultado = %d \n\r", multiplicacion(1000,-1));
+
+    printf("Division                - resultado = %d \n\r", division(10000,100));
+
+    printf("Potencia                - resultado = %d \n\r", exponencial(5,0));
+
+    printf("Sumatoria de cuadrados  - resultado = ");sumatoria(20);
     return 0;
 
 }
-
